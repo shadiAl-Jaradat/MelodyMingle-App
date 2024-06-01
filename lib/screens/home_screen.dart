f27ec027dca866fc14fffd500dd3494aa7c27bdc
Shadi Al-Jaradat <94618324+shadiAl-Jaradat@users.noreply.github.com>
2024-06-01 16:26:55 +0300

    fix issue in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:music_player_app/core/controllers.dart';
 import 'package:music_player_app/core/spotify_service.dart';
 import 'package:music_player_app/models/artist.dart';
@@ -77,6 +79,129 @@ class _HomeScreenState extends State<HomeScreen> {
                     )),
                   ),
                   // Add other widgets and features as required
+                  Obx(() => audioPlayerController.song.value != null
+                      ? Positioned(
+                          bottom: 4,
+                          right: 5,
+                          left: 5,
+                          child: GestureDetector(
+                            onTap: () {
+                              Get.toNamed('/now_playing')?.then((value) => setState(() {}));
+                            },
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.all(Radius.circular(10)),
+                              child: BackdropFilter(
+                                filter: ImageFilter.blur(sigmaX: 28, sigmaY: 28),
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    color: MyColors.tertiaryColor.withOpacity(0.12),
+                                  ),
+                                  child: ListTile(
+                                    leading: Transform.translate(
+                                      offset: const Offset(-4.0, 3.0),
+                                      child: Container(
+                                        width: 44,
+                                        height: 44,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(8),
+                                          image: DecorationImage(
+                                            image: NetworkImage(audioPlayerController.song.value?.imageUrl ?? ''),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    trailing: Transform.translate(
+                                      offset: const Offset(18.0, 0.0),
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          InkWell(
+                                            onTap: () async {
+                                              if (audioPlayerController.isPlaying.value) {
+                                                await audioPlayerController.pause();
+                                              } else {
+                                                await audioPlayerController.play();
+                                              }
+                                              setState(() {});
+                                            },
+                                            child: Obx(
+                                              () => Icon(
+                                                audioPlayerController.isPlaying.value ? Icons.pause : Icons.play_arrow,
+                                                color: MyColors.tertiaryColor,
+                                                size: 38,
+                                              ),
+                                            ),
+                                          ),
+                                          InkWell(
+                                            onTap: () async {
+                                              if (audioPlayerController.inSearchMode.value) {
+                                                if (audioPlayerController.currentIndex.value !=
+                                                    audioPlayerController.searchedSongs.length - 1) {
+                                                  audioPlayerController.setCurrentIndex(audioPlayerController.currentIndex.value + 1);
+
+                                                  Song newSong =
+                                                      audioPlayerController.searchedSongs[audioPlayerController.currentIndex.value];
+                                                  audioPlayerController.setSong(newSong);
+                                                  await audioPlayerController.play();
+                                                  setState(() {});
+                                                }
+                                              } else {
+                                                if (audioPlayerController.currentIndex.value !=
+                                                    audioPlayerController.listOfSongs.length - 1) {
+                                                  audioPlayerController.setCurrentIndex(audioPlayerController.currentIndex.value + 1);
+                                                  Song newSong =
+                                                      audioPlayerController.listOfSongs[audioPlayerController.currentIndex.value];
+                                                  audioPlayerController.setSong(newSong);
+                                                  await audioPlayerController.play();
+                                                  setState(() {});
+                                                }
+                                              }
+                                            },
+                                            child: Obx(
+                                              () => audioPlayerController.inSearchMode.value
+                                                  ? Icon(
+                                                      Icons.skip_next,
+                                                      color: audioPlayerController.currentIndex.value !=
+                                                              audioPlayerController.searchedSongs.length - 1
+                                                          ? MyColors.tertiaryColor
+                                                          : MyColors.tertiaryColor.withOpacity(0.4),
+                                                      size: 38,
+                                                    )
+                                                  : Icon(
+                                                      Icons.skip_next,
+                                                      color: audioPlayerController.currentIndex.value !=
+                                                              audioPlayerController.listOfSongs.length - 1
+                                                          ? MyColors.tertiaryColor
+                                                          : MyColors.tertiaryColor.withOpacity(0.4),
+                                                      size: 38,
+                                                    ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    title: Text(
+                                      audioPlayerController.song.value?.name ?? '',
+                                      style: GoogleFonts.poppins(
+                                        color: MyColors.tertiaryColor,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      : SizedBox(
+                          child: Text('${audioPlayerController.currentIndex.string} ${audioPlayerController.song.value?.artist ?? ''}'),
+                        ))
                 ],
               ),
               bottomNavigationBar: CustomTabBar(
